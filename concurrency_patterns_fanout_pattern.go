ec2f6a8de043b069f80379ecbbe601267aa856a5
Giacomo Quinalia <giacomo.quinalia@ixcsoft.com.br>
2024-06-04 10:07:00 -0300

    giacomo

--- concurrency_patterns_fanout_pattern.go
+++ concurrency_patterns_fanout_pattern.go
@@ -14,20 +14,6 @@ func publish(output chan<- int, value int, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
-func fanOut(input <-chan int, outputs ...chan<- int) {
-	var wg sync.WaitGroup
-	for v := range input {
-		wg.Add(len(outputs))
-		for _, saida := range outputs {
-			go publish(saida, v, &wg)
-		}
-		wg.Wait()
-	}
-	for _, output := range outputs {
-		close(output)
-	}
-}
-
 func sequence(start, end int) <-chan int {
 	input := make(chan int)
 	go func() {
@@ -39,25 +25,35 @@ func sequence(start, end int) <-chan int {
 	return input
 }
 
-// START OMIT
-func main() {
-	ch1, ch2 := make(chan int), make(chan int)
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go worker(1, ch1, &wg)
-	go worker(2, ch2, &wg)
-
-	fanOut(sequence(1, 10), ch1, ch2)
-	wg.Wait()
-}
-
 func worker(id int, in <-chan int, wg *sync.WaitGroup) {
 	for v := range in {
 		fmt.Printf("Worker %d está processando o valor %d\n", id, v)
 	}
 	wg.Done()
+}
+
+// START OMIT
+func main() {
+	ch1, ch2 := make(chan int), make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go worker(1, ch1, &wg)
+	go worker(2, ch2, &wg)
+	fanOut(sequence(1, 10), ch1, ch2)
+	wg.Wait()
+}
+func fanOut(input <-chan int, outputs ...chan<- int) {
+	var wg sync.WaitGroup
+	for v := range input {
+		wg.Add(len(outputs))
+		for _, output := range outputs {
+			go publish(output, v, &wg)
+		}
+		wg.Wait()
+	}
+	for _, output := range outputs {
+		close(output)
+	}
 }
 
 // END OMIT
